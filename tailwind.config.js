3339fca825439eaf96db8943d3046663e640946c
Chandler De Angelis <chandler.dea@me.com>
2024-01-13 15:25:52 -0700

    Stying pricing section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'presence-purple-100': '#E2CFFC',
+        'presence-purple-200': '#C49EFA',
+        'presence-purple-300': '#9A5EED',
+        'presence-purple-400': '#7F3CDD',
+        'presence-purple-500': '#6628BD',
+        'presence-purple-600': '#5B24A8',
+        'presence-purple-700': '#381070',
+        'presence-purple-800': '#210647',
+        'presence-purple-900': '#160330',
+      }
+    },
   },
   plugins: [],
 }
